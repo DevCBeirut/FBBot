6fd30c9930821c8c8c51b8c6f7a927a2baa63ad8
Wallpublisher <salah.awad@wallpublisher.com>
2018-12-17 12:02:36 +0200

    Adding initial code
    First push

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express();
+var bodyParser = require('body-parser');
+const config = require('./config/config.js');
+
+process.env.NODE_ENV = 'development';
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+var port = process.env.PORT || global.gConfig.node_port;
+var router = express.Router();
+
+router.use(function (req, res, next) {
+    console.log('API is called.');
+    next();
+});
+
+
+router.get('/', function (req, res) {
+    res.json({
+        message: 'Welcome to Bot API'
+    });
+});
+
+router.route('/fbbot')
+    .get((req, resp) => {
+        resp.json({
+            message: 'BOTT'
+        });
+    })
+    .post((req, resp) => {
+        if (!validateRequest(req.body)) {
+            console.log('wrong request');
+        }
+
+        resp.json({
+            message: 'BOTT'
+        });
+    });
+
+app.use('/api', router);
+app.on('error', onError);
+app.listen(port);
+console.log('new server created on port ' + port);
+function onError(error) {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+  
+    var bind = typeof port === "string"
+      ? "Pipe " + port
+      : "Port " + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+function validateRequest(body) {
+
+}
